8bf77ac4f82212dd74d16958ef20ef62ec041164
dtkaplan <dtkaplan@gmail.com>
2017-04-12 15:37:05 -0500

    Added confidence intervals to props()

--- R/counts.R
+++ R/counts.R
@@ -1,4 +1,6 @@
 #' Counts and proportions
+#' @param level set to, e.g. 0.95 for the confidence intervals
+#' to be generated.
 
 #' @export
 counts <- function(formula, data, ..., tidy = FALSE) {
@@ -9,10 +11,24 @@ counts <- function(formula, data, ..., tidy = FALSE) {
 }
 
 #' @export
-props <- function(formula, data, as.percent = FALSE, ..., tidy = FALSE) {
+props <- function(formula, data, as.percent = FALSE, level = NULL, ..., tidy = FALSE) {
   fmt <- ifelse(as.percent, "percent", "proportion")
   Tmp <- mosaic::tally(formula, data, format = fmt, ...)
-  if (tidy) Tmp <- as.data.frame(Tmp)
+  Tmp <- as.data.frame(Tmp)
+  names(Tmp)[2] <- "proportion"
+  if ( !is.null(level)) {
+    # add the low and high confidence intervals
+    Tmp$conf.low <- qbinom((1-level)/2, size = nrow(data),
+                           prob = Tmp$proportion)/nrow(data)
+    Tmp$conf.high <- qbinom(1-(1-level)/2, size = nrow(data),
+                            prob = Tmp$proportion)/nrow(data)
+  }
+
+  if ( !tidy ) {
+    foo <- t(Tmp[, -1])
+    colnames(foo) <- Tmp[, 1]
+    Tmp <- foo
+  }
 
   Tmp
 }
